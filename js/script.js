5915d6cc16fca40f7abac10066e6ebbc32dac34f
Lia <aliyahrenomeron7@gmail.com>
2026-03-28 14:58:56 -0400

    added cursor and button for future use

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let startBtn = document.getElementById("start-btn");
+
+startBtn.addEventListener("click", openTapes);
+
+function openTapes() {
+    console.log("Button is clicked");
+    window.location.href = "#";
+}
