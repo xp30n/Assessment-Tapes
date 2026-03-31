41cf7b686ef184a0fdbca9464dde364896564714
Lia <aliyahrenomeron7@gmail.com>
2026-03-31 19:58:39 -0400

    added logo image and video

--- js/Tapes.js
+++ js/Tapes.js
@@ -1,29 +1,46 @@
-let skinBtn = document.getElementById("skin-btn");
-let mouthBtn = document.getElementById("mouth-btn");
-let eyesBtn = document.getElementById("eyes-btn");
-let speechBtn = document.getElementById("speech-btn");
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::
+// ::                  BUTTON IDS                     ::
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-let buttons = document.getElementById("vhs-btns");
+const skinBtn = document.getElementById("skin-btn");
+const mouthBtn = document.getElementById("mouth-btn");
+const eyesBtn = document.getElementById("eyes-btn");
+const speechBtn = document.getElementById("speech-btn");
+
+const buttons = document.getElementById("vhs-btns");
+
+const vhsPlayer = document.getElementById("vhs-player");
+
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::
+// ::                 EVENT LISTENERS                 ::
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 skinBtn.addEventListener("click", greenButton);
 mouthBtn.addEventListener("click", yellowButton);
 eyesBtn.addEventListener("click", orangeButton);
 speechBtn.addEventListener("click", redButton);
 
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::
+// ::                  FUNCTIONS                      ::
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 function greenButton() {
     console.log("GREEN BUTTON IS CLICKED");
+    vhsPlayer.play();
 }
 
 function yellowButton() {
     console.log("YELLOW BUTTON IS CLICKED");
+    vhsPlayer.play();
 }
 
 function orangeButton() {
     console.log("ORANGE BUTTON IS CLICKED");
+    vhsPlayer.play();
 }
 
 function redButton() {
     console.log("RED BUTTON IS CLICKED");
+    vhsPlayer.play();
 }
 
-// ! ADD ENTRANCE TAPE PAGE
