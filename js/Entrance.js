6134de85b0ac84d174d9bc1676b1f45d8bf87348
Lia <aliyahrenomeron7@gmail.com>
2026-03-31 22:15:33 -0400

    added a temporary button to proceed

---
+++ js/Entrance.js
@@ -0,0 +1,6 @@
+let nextBtn = document.getElementById("temp-btn");
+
+nextBtn.addEventListener("click", function() {
+    console.log("Button is clicked");
+    window.location.href = "../html/Tapes.html";
+})
