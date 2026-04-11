c5500dad97b9987d96e679a73b2f7a1d3da4214f
Lia <aliyahrenomeron7@gmail.com>
2026-04-11 18:30:38 -0400

    Added the welcome page video and play btn

--- js/Entrance.js
+++ js/Entrance.js
@@ -1,6 +1,14 @@
 let nextBtn = document.getElementById("temp-btn");
+let entranceTape = document.getElementById("entrance-tape");
+let playBtn = document.getElementById("play-button");
 
 nextBtn.addEventListener("click", function() {
     // console.log("Button is clicked");
     window.location.href = "../html/Tapes.html";
 })
+
+playBtn.addEventListener("click", function() {
+    entranceTape.play();
+});
+
+// ! ADD ENTRANCE TAPE PAGE 
