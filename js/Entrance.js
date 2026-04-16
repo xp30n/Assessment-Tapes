1e024251447c7beb088c8ab92d2437b428ded744
Lia <aliyahrenomeron7@gmail.com>
2026-04-16 11:00:47 -0400

    added new typewriter function and spacebar interactions

--- js/Entrance.js
+++ js/Entrance.js
@@ -1,26 +1,59 @@
-const nextPageBtn = document.getElementById("next-btn");
-nextPageBtn.addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", () => {
+  const nextPageBtn = document.getElementById("next-btn");
+  nextPageBtn.addEventListener("click", function () {
     console.log("Button is clicked");
-})
+  });
 
-const entranceEl = document.getElementById("title-el");
+  const entranceEl = document.getElementById("title-el");
 
-const entranceText = "WELCOME TO THE ENTRANCE TAPES";
-const speed = 200;
-let i = 0;
+  const entranceText = [
+    "WELCOME TO THE ENTRANCE TAPES",
+    "CORPUS CONSUMPTION COMPULSION TRANSFIGURATION,",
+    "ALSO KNOWN AS C.C.C.T.,",
+    "IS THE RESEARCH OF CANNIBALISTIC SUPERNATURAL CHANGES",
+    "WITHIN HUMANS.",
+    "THIS TAPE IS TO INTRODUCE YOU TO THIS THE RESEARCH",
+    "CONDUCTED BY THE SPEAR OF HERMES SUPERNATURAL RESEARCH CENTRE.",
+  ];
 
-entranceEl.textContent = "";
+  const speed = 100;
 
-function typeWriter() {
-  if (i < entranceText.length) {
-    entranceEl.innerHTML += entranceText.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  } else {
-    setTimeout(() => {
-        // window.location.href = "Welcome.html";
-    }, 2000)
+  let lineIndex = 0;
+  let charIndex = 0;
+  let isTyping = false;
+
+  entranceEl.innerHTML = "";
+
+  console.log(entranceEl)
+
+  function typeLine() {
+    if (lineIndex >= entranceText.length) return;
+
+    isTyping = true;
+
+    function typeChar() {
+      if (charIndex < entranceText[lineIndex].length) {
+        entranceEl.innerHTML += entranceText[lineIndex][charIndex];
+        charIndex++;
+        setTimeout(typeChar, speed);
+      } else {
+        entranceEl.innerHTML += "<br><br>";
+        charIndex = 0;
+        lineIndex++;
+        isTyping = false;
+      }
+    }
+
+    typeChar();
   }
-}
 
-typeWriter();
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Space" || e.key === " ") {
+      e.preventDefault();
+
+      if (!isTyping) {
+        typeLine();
+      }
+    }
+  });
+});
