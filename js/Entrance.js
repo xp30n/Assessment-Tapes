ab8d56c957e2ea239e320aaf38ee0e6b531f33b0
Lia <aliyahrenomeron7@gmail.com>
2026-04-15 21:26:28 -0400

    added typewriter function to entrance page

--- js/Entrance.js
+++ js/Entrance.js
@@ -2,3 +2,25 @@ const nextPageBtn = document.getElementById("next-btn");
 nextPageBtn.addEventListener("click", function() {
     console.log("Button is clicked");
 })
+
+const entranceEl = document.getElementById("title-el");
+
+const entranceText = "WELCOME TO THE ENTRANCE TAPES";
+const speed = 200;
+let i = 0;
+
+entranceEl.textContent = "";
+
+function typeWriter() {
+  if (i < entranceText.length) {
+    entranceEl.innerHTML += entranceText.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(() => {
+        // window.location.href = "Welcome.html";
+    }, 2000)
+  }
+}
+
+typeWriter();
